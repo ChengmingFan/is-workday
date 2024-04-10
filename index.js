3e644f1c98cb5cf7544da80712427d3c90da8fa6
chengming.fan <chengming.fan@thoughtworks.com>
2024-04-10 13:04:57 +0800

    add more function

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function isWorkday(date, locale) {
     // Check if it's a holiday
     const formattedDate = dt.toISODate();
     return !holidays.includes(formattedDate);
-
 }
 
 function update(newLocale, newHolidays) {
@@ -22,8 +21,64 @@ function update(newLocale, newHolidays) {
     holidays = [...holidays, ...newHolidays]; // Add new holidays to the existing list
 }
 
+function getNextWorkday(date, locale) {
+    let dt = DateTime.fromJSDate(date, { locale });
+    dt = dt.plus({ days: 1 });
+    while (!isWorkday(dt.toJSDate(), locale)) {
+        dt = dt.plus({ days: 1 });
+    }
+    return dt.toJSDate();
+}
+
+function isHoliday(date, locale) {
+    const dt = DateTime.fromJSDate(date, { locale });
+    const formattedDate = dt.toISODate();
+    return holidays.includes(formattedDate);
+}
+
+function addHoliday(date) {
+    const formattedDate = DateTime.fromJSDate(date).toISODate();
+    if (!holidays.includes(formattedDate)) {
+        holidays.push(formattedDate);
+    }
+}
+
+function removeHoliday(date) {
+    const formattedDate = DateTime.fromJSDate(date).toISODate();
+    const index = holidays.indexOf(formattedDate);
+    if (index !== -1) {
+        holidays.splice(index, 1);
+    }
+}
+
+function getWorkdaysInRange(startDate, endDate, locale) {
+    const workdays = [];
+    let currentDate = DateTime.fromJSDate(startDate, { locale });
+    const end = DateTime.fromJSDate(endDate, { locale });
+
+    while (currentDate <= end) {
+        if (isWorkday(currentDate.toJSDate(), locale)) {
+            workdays.push(currentDate.toJSDate());
+        }
+        currentDate = currentDate.plus({ days: 1 });
+    }
+
+    return workdays;
+}
+
+function getHolidayName(date, locale) {
+    const dt = DateTime.fromJSDate(date, { locale });
+    const formattedDate = dt.toISODate();
+    return holidays[formattedDate];
+}
+
 module.exports = {
     isWorkday,
-    update
+    update,
+    getNextWorkday,
+    isHoliday,
+    addHoliday,
+    removeHoliday,
+    getWorkdaysInRange,
+    getHolidayName
 };
-
